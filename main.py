8368c67cf8c402d1f6e6d87b94cae94e25120ee0
Dean Rice <deanrice44@gmail.com>
2024-02-05 15:40:59 -0500

    Barebones bot

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import configparser
+
+cfg = configparser.ConfigParser()
+cfg.read("token.ini")
+TOKEN = cfg["SECRET"]["token"]
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}!")
+
+    async def on_message(self, message):
+        print(f"Message recieved from {message.author}: {message.content}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(TOKEN)
