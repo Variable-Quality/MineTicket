5d5a339f913535f4b92524d036d6ea300b033e40
Arturo Martinez <41694941+2012art@users.noreply.github.com>
2024-03-25 05:14:32 -0400

    Update to json_parsing.py
    Did some research on the json (j son) files. Here's my rough draft to it and I haven't tested it yet.

--- json_parsing.py
+++ json_parsing.py
@@ -1,17 +1,62 @@
+import discord
 import json
 import sql_interface as sql
 
-class message():
+# lets assume that a channel called #intake,
+# in the category called "Tickets" will be made,
+# make sure JSONParser only reads from there
 
-    def __init__(self, message):
-        json_format = json.loads(message.content)
-        self.message_object = message
-        self.event = json_format["event"]
-        self.player_id = json_format["discord-id"]
-        #Not sure what to do with this atm but I would imagine its something ingame
-        #So for now it has a place in the message object
-        self.player_uuid = json_format["player-uuid"]
-        self.id = int(json_format["id"])
-        self.message = json_format["message"]
+class ParseJSON():
+    def __init__(self, guild):
+        self.guild = guild
 
-        self.active_entry = sql.fetch_by_id(self.id)
+    async def check_json_messages(self, client):
+        category_name = "Tickets"
+        channel_name = "intake"
+
+        #check if setup was performed
+        category = discord.utils.get(self.guild.categories, name = category_name)
+        if not category:
+            print(f"Category '{category_name}' not found in guild '{self.guild.name}")
+            return
+        channel = discord.utils.get(category.text_channels, name = channel_name)
+        if not channel:
+            print(f"Channel '{channel_name}' not found in category '{category_name}'")
+            return
+        # wait i got tired here what am i doing
+        # help
+
+    # ok time to read JSON
+    async def parse_json_message(self, message):
+        try:
+            json_data = json.loads(message.content)
+            self.message_object = message
+            self.event = json_data["event"]
+            self.player_id = json_data["discord-id"]
+            #Not sure what to do with this atm but I would imagine its something ingame
+            #So for now it has a place in the message object
+            self.player_uuid = json_data["player-uuid"]
+            self.id = int(json_data["id"])
+            self.message = json_data["message"]
+            
+        except json.JSONDecodeError:
+            return # Skip if not JSON
+        
+        # The event does one of the three Cs
+        event = json_data.get("event")
+        if event == "create":
+            return
+        elif event == "claim":
+            return
+        elif event == "close":
+            return
+     
+    # ok now do magic with the events   
+    async def create_event(self, json_data):
+        pass
+
+    async def claim_event(self, json_data):
+        pass
+
+    async def close_event(self, json_data):
+        pass
